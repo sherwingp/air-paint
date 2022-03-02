bfeab8db82d9804df8e9f19b57abf004674c816e
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-03-02 18:01:07 +0000

    Add co-ordinate tracking for index finger tip

--- hand_tracker.py
+++ hand_tracker.py
@@ -4,6 +4,14 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10,100)
+fontScale              = 1
+fontColor              = (0,0,0)
+thickness              = 1
+lineType               = 2
+
 # For webcam input:
 cap = cv2.VideoCapture(1)
 with mp_hands.Hands(
@@ -23,16 +31,29 @@ with mp_hands.Hands(
     image.flags.writeable = True
     results = hands.process(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    annotated_image = image.copy()
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        
+        image_height, image_width, _ = image.shape
+
+        # Print index finger tip coordinates.
+        cv2.putText(annotated_image, f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, {hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})', 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
+
         mp_drawing.draw_landmarks(
-            image,
+            annotated_image,
             hand_landmarks,
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    cv2.imshow('MediaPipe Hands', cv2.flip(annotated_image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
--- paint.py
+++ paint.py
@@ -2,6 +2,7 @@ from tkinter import *
 import hand_tracker
 
 class PaintApp:
+    drawing_tool = "draw"
     # Catch index up
     # Catch middle up
     # Draw pencil
