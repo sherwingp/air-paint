0bbe2066a4b606334bfbe818b797a19d14a7d0b1
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-03-02 15:09:55 +0000

    Add drawing for hand tracking

--- hand_tracker.py
+++ hand_tracker.py
@@ -5,7 +5,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 # For webcam input:
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
@@ -14,9 +14,25 @@ with mp_hands.Hands(
     success, image = cap.read()
     if not success:
       print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
       continue
 
-    cv2.imshow('tracker', image)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    # Convert BGR image to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    results = hands.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
