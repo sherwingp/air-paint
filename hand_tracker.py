363295f9d4b148b52f18f59abbc4fa6fcd7a4460
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-03-02 05:14:03 +0000

    Add camera live stream

--- hand_tracker.py
+++ hand_tracker.py
@@ -3,3 +3,20 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    cv2.imshow('tracker', image)
+    if cv2.waitKey(1) == ord('q'):
+        break
