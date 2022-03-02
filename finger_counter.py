1c8bf5d931ec88fb6d8b9ab7a45492e12f256759
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-03-02 19:13:52 +0000

    Add paint mode selection

--- finger_counter.py
+++ finger_counter.py
@@ -22,7 +22,6 @@ def get_mode(image, results):
                         'RIGHT_PINKY': False, 'LEFT_INDEX': False, 'LEFT_MIDDLE': False,
                         'LEFT_RING': False, 'LEFT_PINKY': False}
     
-    
     # Iterate over the found hands in the image.
     for hand_index, hand_info in enumerate(results.multi_handedness):
         
@@ -46,6 +45,13 @@ def get_mode(image, results):
                 
                 # Increment the count of the fingers up of the hand by 1.
                 count[hand_label.upper()] += 1
+
+    # Write the mode   
+    fs = fingers_statuses
+    mode = None
+    if (fs['RIGHT_INDEX'] == True or fs['LEFT_INDEX'] == True) and count['RIGHT'] + count['LEFT'] == 1:
+        mode = 'Paint'
+    cv2.putText(output_image, f"Mode: {mode}", (10, 25),cv2.FONT_HERSHEY_COMPLEX, 1, (20,255,155), 2)
 
     # Return the output image, the status of each finger and the count of the fingers up of both hands.
     return output_image
