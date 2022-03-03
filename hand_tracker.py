18ec99260297ca0b2e30a180e74ede72aeea000b
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-03-03 00:40:10 +0000

    Move OpenCV video stream to Tkinter canvas

--- hand_tracker.py
+++ hand_tracker.py
@@ -6,51 +6,57 @@ class MyVideoCapture:
   def __init__(self, video_source=1):
     # Open video source
     self.cap = cv2.VideoCapture(1)
+    self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
-    mp_hands = mp.solutions.hands
+  def get_frame(self):
+    if self.cap.isOpened():
+        ret, frame = self.cap.read()
+        if ret:
+            # Return a boolean success flag and the current frame converted to BGR
+            return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        else:
+            return (ret, None)
+    else:
+        return (ret, None)
 
+    # mp_drawing = mp.solutions.drawing_utils
+    # mp_drawing_styles = mp.solutions.drawing_styles
+    # mp_hands = mp.solutions.hands
 
-    font                   = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (10,100)
-    fontScale              = 1
-    fontColor              = (0,0,0)
-    thickness              = 1
-    lineType               = 2
+    # with mp_hands.Hands(
+    #     model_complexity=0,
+    #     min_detection_confidence=0.5,
+    #     min_tracking_confidence=0.5) as hands:
+    #   while self.cap.isOpened():
+    #     success, image = self.cap.read()
+    #     if not success:
+    #       print("Ignoring empty camera frame.")
+    #       continue
 
-    with mp_hands.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
-      while self.cap.isOpened():
-        success, image = self.cap.read()
-        if not success:
-          print("Ignoring empty camera frame.")
-          continue
-
-        # Convert BGR image to RGB
-        image = cv2.flip(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), 1)
+    #     # Convert BGR image to RGB
+    #     image = cv2.flip(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), 1)
         
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        results = hands.process(image)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        annotated_image = image.copy()
-        if results.multi_hand_landmarks:
-          for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                annotated_image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
+    #     # Draw the hand annotations on the image.
+    #     image.flags.writeable = True
+    #     results = hands.process(image)
+    #     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    #     annotated_image = image.copy()
+    #     if results.multi_hand_landmarks:
+    #       for hand_landmarks in results.multi_hand_landmarks:
+    #         mp_drawing.draw_landmarks(
+    #             annotated_image,
+    #             hand_landmarks,
+    #             mp_hands.HAND_CONNECTIONS,
+    #             mp_drawing_styles.get_default_hand_landmarks_style(),
+    #             mp_drawing_styles.get_default_hand_connections_style())
 
-          annotated_image, cursor_pos, mode = fc.get_mode(annotated_image, results)
+    #       annotated_image, cursor_pos, mode = fc.get_mode(annotated_image, results)
 
-        # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Hands', annotated_image)
+    #     if cv2.waitKey(5) & 0xFF == 27:
+    #       break
 
-        if cv2.waitKey(5) & 0xFF == 27:
-          break
-    self.cap.release()
+    def __del__(self):
+      if self.cap.isOpened():
+        self.cap.release()
+        cv2.destroyAllWindows()
--- paint.py
+++ paint.py
@@ -1,15 +1,34 @@
-import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
 from hand_tracker import MyVideoCapture
+import cv2
 
 class PaintApp:
-    def __init__(self, window, window_title, video_source=0):
+    def __init__(self, window, window_title, video_source=1):
         self.window = window
         self.window.title(window_title)
-        self.video_source = video_source
+        self.video_source = video_source    
+        self.vid = MyVideoCapture()
 
-        self.vid = MyVideoCapture(self.video_source)
+        self.canvas = Canvas(self.window, width=self.vid.width, height = self.vid.height)
+
+        self.canvas.pack()
+
+        self.delay = 15
+        self.update()
+        self.window.mainloop()
+
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = NW)
+
+        self.window.after(self.delay, self.update)
 
     def draw(image, x_pos, y_pos, mode):
         drawing_tool = "draw"
 
-PaintApp(tkinter.Tk(), "Tkinter and OpenCV")
+PaintApp(Tk(), "Air Paint", 1)
